6692d8579949b16265416bc7196e61a862af161b
Marion Poljakov <marion.poljakov@gmail.com>
2024-11-14 11:17:14 +0200

    Database, main files and testing the database

--- src/main/java/com/myweb/javaweb/MainController.java
+++ src/main/java/com/myweb/javaweb/MainController.java
@@ -1,4 +1,14 @@
 package com.myweb.javaweb;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("")
+    public String showHomePage () {
+        System.out.println("Home page");
+        return "index";
+    }
 }
